4d862dd26b25e3a2ec98730c84a7eaac53e88d13
under <poke2000@live.jp>
2020-01-01 00:41:42 +0900

    kuso

---
+++ cogs/loops.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from datetime import datetime 
+
+prev_time = 0
+GUILD_ID = int(os.environ["GUILD_ID"])
+CATEGORY_ID = int(os.environ["CATEGORY_ID"])
+MES = os.environ["MES"]
+timeChannel = None
+
+class loops(commands.Cog):
+
+    def __init__(self, bot, timeChannel):
+        self.bot = bot
+        self.timeChannel = timeChannel
+        self.loops.start()
+
+    @tasks.loop(seconds=5)
+    async def loops(self):
+        global prev_time
+        # 現在の時刻
+        now = datetime.now().strftime('%H:%M')
+        if now != prev_time:
+            await timeChannel.edit(name=now)
+
+
+def setup(bot):
+    bot.add_cog(loops(bot, timeChannel))
+
+
--- main.py
+++ main.py
@@ -4,48 +4,38 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 import asyncio
+from discord.ext import commands
+from cogs import loops 
 
 prev_time = 0
-timeChannel = None
-
-TOKEN = os.environ["TOKEN"]
 GUILD_ID = int(os.environ["GUILD_ID"])
 CATEGORY_ID = int(os.environ["CATEGORY_ID"])
 MES = os.environ["MES"]
+timeChannel = None
+
+TOKEN = os.environ["TOKEN"]
+
 # 接続に必要なオブジェクトを生成
-client = discord.Client()
+bot = commands.Bot()
 
-# @tasks.loop(seconds=5)
-async def loop():
-    global prev_time
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now != prev_time:
-        await timeChannel.edit(name=now)
-    await asyncio.sleep(10)
-
-#ループ処理実行
-@client.event
+@bot.event
 async def on_ready():
     await init()
-    asyncio.ensure_future(loop())
 
 async def init():
-    global timeChannel
-    guild = client.get_guild(GUILD_ID)
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        guild.me: discord.PermissionOverwrite(read_messages=False)
-    }
-    c = client.get_channel(CATEGORY_ID)
-    ch = client.get_all_channels()
-    for i in ch:
-        if i.category == c:
-            await i.delete()
+        global timeChannel
+        guild = bot.get_guild(GUILD_ID)
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=False)
+        }
+        c = bot.get_channel(CATEGORY_ID)
+        ch = bot.get_all_channels()
+        for i in ch:
+            if i.category == c:
+                await i.delete()
 
-    timeChannel = await guild.create_text_channel('Time', overwrites=overwrites, category=c)
-    await guild.create_text_channel(MES, overwrites=overwrites, category=c)
-
-if __name__ == "__main__":
-    # Botの起動とDiscordサーバーへの接続
-    client.run(TOKEN)
+        timeChannel = await guild.create_text_channel('Time', overwrites=overwrites, category=c)
+        await guild.create_text_channel(MES, overwrites=overwrites, category=c)
+       
+bot.run(TOKEN)
